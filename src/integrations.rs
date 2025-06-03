1530c373fdf23b0cc87434a8cd3e746a3afd5658
jackjohn7 <branchjohnchristopher@gmail.com>
2025-06-03 03:45:23 -0500

    parse one or many paths

--- src/integrations.rs
+++ src/integrations.rs
@@ -1,11 +1,14 @@
 use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, OneOrMany};
 
 /// Contains information for all things related to programmability and
 /// external integrations in Basalt.
+#[serde_as]
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Integrations {
     /// Paths to files that will be executed on server events
+    #[serde_as(as = "OneOrMany<_>")]
     pub events: Vec<PathBuf>,
 }
